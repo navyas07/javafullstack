06be3c3f03317ec1c6ebdb069d6f2415c2a778de
navyas07 <navyasuresh712@gmail.com>
2025-08-01 20:51:49 +0530

    basic programs

---
+++ Assignment/src/com/training/basic/Odd.java
@@ -0,0 +1,14 @@
+package com.training.basic;
+
+public class Odd {
+
+	public static void main(String[] args) {
+		System.out.println("The odd numbers are ");
+		for (int i = 0; i <= 20; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
